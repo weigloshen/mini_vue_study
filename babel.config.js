666f1db7723577e2f5a26bfb8de06b73f9d14e3f
weigloshen <weishusheng100@gmail.com>
2023-05-16 19:44:58 +0800

    init

---
+++ babel.config.js
@@ -0,0 +1,9 @@
+/**
+ * jest files use ESMODULE and TYPESCRIPT
+ */
+module.exports = {
+  presets: [
+    ["@babel/preset-env", { targets: { node: "current" } }],
+    "@babel/preset-typescript",
+  ],
+};
